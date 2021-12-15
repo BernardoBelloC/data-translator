6cd2e1488ca79d964eddae0eb8a78ff662623b6c
Bernardo Bello <berbel@deacero.com>
2021-12-15 08:07:11 -0600

    jksadh

--- 14.12.2021/part4.py
+++ 14.12.2021/part4.py
@@ -11,6 +11,7 @@ from dash.dependencies import Input, Output
 url = "https://raw.githubusercontent.com/chyld/datasets/main/auto-mpg.csv" # Make sure the url is the raw version of the 
 download = requests.get(url).content
 df = pd.read_csv(io.StringIO(download.decode('utf-8')),error_bad_lines=False,engine='python')
+df['horsepower']=pd.to_numeric(df['horsepower'],errors='coerce')
 
 def label_nice (row):
     if row['cylinders'] == 4:
@@ -28,15 +29,14 @@ all_dims = ['4 cylinders', '6 cylinders',
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
-    dcc.Graph(id='graph-with-dropdown'),
+    html.H1("Horsepower by cylinders"),
     dcc.Dropdown(
-        id="dropdown",
-        options=[{"label": x, "value": x} 
-                 for x in all_dims],
-        value='8 cylinders'
-        
-    
+    id="dropdown",
+    options=[{"label": x, "value": x} for x in all_dims],
+    value='8 cylinders'
     ),
+    dcc.Graph(id='graph-with-dropdown'),
+
 ])
 
 
